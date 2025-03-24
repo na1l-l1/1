0f78ef0eabaa541c28b1663440e669a3620c37ad
Nail <nailbest001@gmail.com>
2025-03-24 19:29:42 +0500

    5

--- app/src/main/java/com/example/myapplication/CountryAdapter.java
+++ app/src/main/java/com/example/myapplication/CountryAdapter.java
@@ -3,8 +3,7 @@ package com.example.myapplication;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
+
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,15 +1,8 @@
 package com.example.myapplication;
-
-
+import android.graphics.Color;
 import android.os.Bundle;
-
-
-
-
-
 import androidx.appcompat.app.AppCompatActivity;
-
-
+import androidx.appcompat.widget.Toolbar;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
 
 
     }
